d4641d83bc1d081ba6beda56801c8d5d1a954df7
hainguyen-eh <nguyenngochai.shipagent@gmail.com>
2024-11-09 19:23:25 +0700

    Update css and js config

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,10 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
+pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+
+pin "bootstrap", to: "https://cdn.skypack.dev/bootstrap@5.3.0-alpha1"
+pin "@popperjs/core", to: "https://cdn.skypack.dev/@popperjs/core@2.11.6"
 pin_all_from "app/javascript/controllers", under: "controllers"
